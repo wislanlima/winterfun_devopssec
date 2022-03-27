e2e0df118892cd4ab18ce999bf0e1624f8fa2427
wislanlima <91568531+wislanlima@users.noreply.github.com>
2022-03-27 13:39:16 +0100

    Creating sonaqube server

--- _terraform/main.tf
+++ _terraform/main.tf
@@ -1,17 +1,13 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
-
   required_version = ">= 0.14.9"
-  
     cloud {
   organization = "NCI2022"
-
   workspaces {
     name = "Terraform_Winterfun"
   }
@@ -131,10 +127,44 @@ resource "aws_instance" "winterfun_prod" {
       "chmod +x /tmp/docker_compose_install.sh",
       "/tmp/docker_compose_install.sh",
     ]
+  }
+}
 
+
+#sonarcube instance creation
+resource "aws_instance" "winterfun_sonarqube" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t3.medium"
+  subnet_id     = "subnet-01f7f9c5b9fc99986"
+  associate_public_ip_address = true
+  key_name = "x21126151_Wislan_Lima"
+  vpc_security_group_ids = ["sg-0268ecbf418effd94"]
+
+  tags = {
+    Name = "winterfun_sonarqube"
   }
 
+  connection {
+    host        = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("x21126151_Wislan_Lima.pem")
+    }
+
+  # copies file from local directory to remote directory
+  provisioner "file" {
+    source      = "docker_compose_install.sh"
+    destination = "/tmp/docker_compose_install.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/docker_compose_install.sh",
+      "/tmp/docker_compose_install.sh",
+    ]
+  }
 }
+
 
 #IP of instances retrieved
 output "winterfun_prod"{
@@ -147,4 +177,8 @@ value = "${aws_instance.winterfun_dev.public_ip}"
 
 output "winterfun_stage"{
 value = "${aws_instance.winterfun_stage.public_ip}"
+}
+
+output "winterfun_sonarqube"{
+value = "${aws_instance.winterfun_sonarqube.public_ip}"
 }
