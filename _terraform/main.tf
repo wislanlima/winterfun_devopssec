88fc50007e785a86125d0283bd2ff51faa78d5ec
wislanlima <91568531+wislanlima@users.noreply.github.com>
2022-04-13 18:19:27 +0100

    Update main.tf

--- _terraform/main.tf
+++ _terraform/main.tf
@@ -1,19 +1,15 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
-
   required_version = ">= 0.14.9"
-
     cloud {
   organization = "NCI2022PROJECT"
-
   workspaces {
-    name = "Winterfun_Project"
+    name = "Terraform_Winterfun"
   }
 }
 }
@@ -25,7 +21,7 @@ provider "aws" {
 
 
 #dev instance creation
-resource "aws_instance" "x21126151_Wislan_Lima_dev" {
+resource "aws_instance" "dev_winterfun" {
   ami           = "ami-08ca3fed11864d6bb"
   instance_type = "t2.medium"
   subnet_id     = "subnet-065588da7b835f2b9"
@@ -34,7 +30,7 @@ resource "aws_instance" "x21126151_Wislan_Lima_dev" {
   vpc_security_group_ids = ["sg-0f18d077b96cfd4fe"]
 
   tags = {
-    Name = "x21126151_Wislan_Lima_dev"
+    Name = "dev_winterfun"
   }
 
   connection {
@@ -62,89 +58,6 @@ resource "aws_instance" "x21126151_Wislan_Lima_dev" {
 
 }
 
-#stage instance creation
-resource "aws_instance" "x21126151_Wislan_Lima_stage" {
-  ami           = "ami-08ca3fed11864d6bb"
-  instance_type = "t2.medium"
-  subnet_id     = "subnet-065588da7b835f2b9"
-  associate_public_ip_address = true
-  key_name = "x21126151_wislan_lima_new"
-  vpc_security_group_ids = ["sg-0f18d077b96cfd4fe"]
-
-  tags = {
-    Name = "x21126151_Wislan_Lima_stage"
-  }
-
-
-  connection {
-    host        = self.public_ip
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = var.pvt_key
-    }
-
-  # copies file from local directory to remote directory
-  provisioner "file" {
-    source      = "docker_compose_install.sh"
-    destination = "/tmp/docker_compose_install.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/docker_compose_install.sh",
-      "/tmp/docker_compose_install.sh",
-    ]
-
-  }
-
-}
-
-
-#prod instance creation
-resource "aws_instance" "x21126151_Wislan_Lima_prod" {
-  ami           = "ami-08ca3fed11864d6bb"
-  instance_type = "t2.medium"
-  subnet_id     = "subnet-065588da7b835f2b9"
-  associate_public_ip_address = true
-  key_name = "x21126151_wislan_lima_new"
-  vpc_security_group_ids = ["sg-0f18d077b96cfd4fe"]
-
-  tags = {
-    Name = "x21126151_Wislan_Lima_prod"
-  }
-
-  connection {
-    host        = self.public_ip
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = var.pvt_key
-    }
-
-  # copies file from local directory to remote directory
-  provisioner "file" {
-    source      = "docker_compose_install.sh"
-    destination = "/tmp/docker_compose_install.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /tmp/docker_compose_install.sh",
-      "/tmp/docker_compose_install.sh",
-    ]
-
-  }
-
-}
-
-#IP of instances retrieved
-output "x21126151_Wislan_Lima_prod"{
-value = "${aws_instance.x21126151_Wislan_Lima_prod.public_ip}"
-}
-
-output "x21126151_Wislan_Lima_dev"{
-value = "${aws_instance.x21126151_Wislan_Lima_dev.public_ip}"
-}
-
-output "x21126151_Wislan_Lima_stage"{
-value = "${aws_instance.x21126151_Wislan_Lima_stage.public_ip}"
+output "dev_winterfun"{
+value = "${aws_instance.dev_winterfun.public_ip}"
 }
