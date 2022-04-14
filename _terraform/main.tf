fc44edbf8a13e42298b710f8078a62d0466df2cd
wislanlima <91568531+wislanlima@users.noreply.github.com>
2022-04-14 11:59:41 +0100

    Create A Server InfluxDB

--- _terraform/main.tf
+++ _terraform/main.tf
@@ -199,6 +199,40 @@ resource "aws_instance" "elk_stack_winterfun" {
   }
 }
 
+#influxdb instance creation
+resource "aws_instance" "influxdb_winterfun" {
+  ami           = "ami-08ca3fed11864d6bb"
+  instance_type = "t3.medium"
+  subnet_id     = "subnet-065588da7b835f2b9"
+  associate_public_ip_address = true
+  key_name = "x21126151_Wislan_Lima"
+  vpc_security_group_ids = ["sg-0f18d077b96cfd4fe"]
+
+  tags = {
+    Name = "influxdb_winterfun"
+  }
+
+  connection {
+    host        = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = var.PVT_KEY
+    }
+
+  # copies file from local directory to remote directory
+  provisioner "file" {
+    source      = "docker_compose_install.sh"
+    destination = "/tmp/docker_compose_install.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/docker_compose_install.sh",
+      "/tmp/docker_compose_install.sh",
+    ]
+  }
+}
+
 #IP of instances retrieved
 output "prod_winterfun"{
 value = "${aws_instance.prod_winterfun.public_ip}"
@@ -218,4 +252,8 @@ value = "${aws_instance.sonarqube_winterfun.public_ip}"
 
 output "elk_stack_winterfun"{
 value = "${aws_instance.elk_stack_winterfun.public_ip}"
+}
+
+output "influxdb_winterfun"{
+value = "${aws_instance.influxdb_winterfun.public_ip}"
 }
