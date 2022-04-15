48f55057d6eec962b0a8465ad238cea827c9f14c
wislanlima <91568531+wislanlima@users.noreply.github.com>
2022-04-15 16:32:37 +0100

    S3 test

--- winterfun/prod_settings.py
+++ winterfun/prod_settings.py
@@ -30,3 +30,16 @@ DATABASES = {
         "PORT": env("PG_PORT"),
     }
 }
+
+USE_S3 = env('USE_S3') == 'TRUE'
+
+if USE_S3:
+    AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_S3_REGION_NAME = 'ue-west-1'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    AWS_S3_VERIFY = True
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
